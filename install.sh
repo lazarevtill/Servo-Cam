11a80b5c2f9f12afbfab6f3c6dab6158aa2dd52a
Till LAzarev <lazarevtill@lazarev.cloud>
2025-10-17 20:47:28 +0200

    Fix

--- install.sh
+++ install.sh
@@ -89,7 +89,9 @@ fi
 
 log "Setting up Python virtual environment"
 if [[ ! -d "${VENV_DIR}" ]]; then
-    python3 -m venv "${VENV_DIR}"
+    python3 -m venv --system-site-packages "${VENV_DIR}"
+else
+    python3 -m venv --system-site-packages "${VENV_DIR}" --upgrade >/dev/null 2>&1 || true
 fi
 
 # shellcheck disable=SC1091
